4cc16c38186f3ffb962309319910c6754552c04e
himanshur0510 <himanshurameshcollege@gmail.com>
2026-07-17 21:01:59 +0530

    Delete sample.cpp

--- sample.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> // Includes the standard input/output library
-
-int main() {
-    // std::cout is used to print text; \n creates a new line
-    std::cout << "Hello, World!\n"; 
-    
-    return 0; // Signals that the program ran successfully
-}
