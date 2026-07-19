fe0ebce95982cf6307ea9e323cd6041f94428926
himanshur0510 <himanshurameshcollege@gmail.com>
2026-07-19 11:03:00 +0530

    Corrected boundary conditions

--- index_of_firstOccurence.cpp
+++ index_of_firstOccurence.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        for(int i=0;i<(haystack).length();i++){
+        if(needle.empty()){
+            return 0;
+        }
+        else{
+        for(int i=0;i<haystack.length();i++){
             int k;
             k=1;
+            
             if(haystack[i]==needle[0]){
                 int y=i;
-                while((needle).length()>k){
-                    y=y+1;
+                if(needle.length()==1){
+                    return i;
+                }
+                else{
+                    while(needle.length()>k){
+                        y=y+1;
+                    if(y>=haystack.length()){
+                        break;
+                    }
                     if(haystack[y]==needle[k]){
-                        k=k+1;
+                        if(k==needle.length()-1){
+                            return i;
+                    }
+                            k=k+1;
                     }
                     else{
                         break;
                     }
-                    if(y>haystack.length()){
-                        break;
-                    }
-                    if(k==needle.length()){
-                        return i;
-                    }
                 }
         }
         }
+        }
         return -1;
-    }
+        }
+    }        
 };
